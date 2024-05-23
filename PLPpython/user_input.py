4808bd13c6c557b8b0366cee424fe96f9b0aa729
DikeH-Ai <onyekachi.okomba@gmail.com>
2024-05-23 12:34:35 -0500

    plp assign 1

---
+++ PLPpython/user_input.py
@@ -0,0 +1,12 @@
+def main():
+    # Ask user for name
+    name = input("What is your name: ")
+    # Ask user for age
+    age = input("What is your age: ")
+    # Ask user for Location
+    location = input("What is your Location: ")
+    print(f"Hello {name}, you are {age} years old and live in {location}")
+
+
+if __name__ == "__main__":
+    main()
